ed7f4770b4b2afd6adf2ac84b8f3d0625b6ddd63
Benjamin Clark <benjaminclark@Benjamins-Mac-mini.local>
2018-08-19 06:01:38 +0930

    Fix IU

--- functions/iu.functions.ts
+++ functions/iu.functions.ts
@@ -1,3 +1,25 @@
+/*
+ * IMMUTABLE UPDATE
+ * ================
+ * 
+ * This immutable object updater leaves the original object in tact and produces a new object.
+ * It targets a specific node and creates a new object with the particular node updated to the new value
+ * 
+ * If the node doesn't already exist, it will be created.
+ * If the node is updated to undefined / null / [] / {}, this will delete the node.
+ * If the parent node no longer has children it will be left as {}.
+ * 
+ * The first param is state - AKA the object to update
+ * The second param is an array representing which node to target.
+ * 
+ * eg. ['lvl1', 'lvl2', 'val'] represents state.lvl1.lvl2.val
+ * 
+ * Note: the second parameter can also be a string delimted by '/'
+ * eg. 'lvl1/lvl2/val'
+ * 
+ * The third param is the value to update the node to
+ */
+
 export function iu(state, subArray, val) {
     if (!subArray) { console.log('No target provided'); } else
         if (subArray.length < 1) { console.log('No target provided'); } else
@@ -11,22 +33,33 @@ function reducer(_state, subArray, val, l) {
     try {
         const key = subArray[l];
         if (l + 1 === subArray.length) {
-            const value = val;
-            const replacer =
-                val === undefined ||
-                    val === null ||
-                    typeof val === 'object' && Array.isArray(val) && val.length === 0 ||
-                    typeof val === 'object' && !Array.isArray(val) && Object.keys(val).length === 0
-                    ? undefined : val;
-            return Object.assign({}, _state, { [key]: replacer });
-        }
-        else {
-            const value = _state[key] ? _state[key] : {};
+            const replacer = isEmpty(val) ? undefined : val;
+            return replacer !== undefined ? 
+                Object.assign({}, _state, { [key]: replacer }) :
+                stateWithoutKey(_state, key)
+        } else {
+            const value = !isEmpty(_state[key]) ? _state[key] : {};
             return Object.assign({}, _state, { [key]: reducer(value, subArray, val, l + 1) });
         }
     } catch (error) { console.log('reducer error =>', error); }
 }
 
 function cloner(arg) {
-    return JSON.parse(JSON.stringify(arg));
+    try {
+        return JSON.parse(JSON.stringify(arg));
+    } catch (e) {
+        return null;
+    }
+}
+
+function isEmpty(val) {
+    return val === undefined ||
+        val === null ||
+        typeof val === 'object' && Array.isArray(val) && val.length === 0 ||
+        typeof val === 'object' && !Array.isArray(val) && Object.keys(val).length === 0
+}
+
+function stateWithoutKey(state, key) {
+    const { [key]: value, ...withoutKey } = state;
+    return withoutKey;
 }
