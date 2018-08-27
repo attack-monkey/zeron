7ec911aae4605fba19b25f64344eda491ee6d732
Benjamin Clark <benjaminclark@Benjamins-Mac-mini.local>
2018-08-28 06:28:13 +0930

    Add cursor CSS to zeron-a element

--- functions/push-state-routes.function.ts
+++ functions/push-state-routes.function.ts
@@ -5,6 +5,7 @@ import { forEach } from './for-each.function';
 export function pushStateRoutes (componentSocketId) {
     const arr = document.querySelectorAll('#' + componentSocketId + ' zeron-a');
     forEach(arr, el => {
+        el.style.cursor = "pointer";
         el.addEventListener('click', () => { 
             const path = el.getAttribute('href');
             route().pushState(path);
