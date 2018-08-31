9e3272c14365001bb608e401232a8606c08945b1
Ben Clark <ben.clark@internode.on.net>
2018-08-31 10:18:33 +0930

    Check pathname before pushing

--- functions/push-state-routes.function.ts
+++ functions/push-state-routes.function.ts
@@ -1,14 +1,14 @@
-
 import { route } from './route.function';
 import { forEach } from './for-each.function';
 
 export function pushStateRoutes (componentSocketId) {
     const arr = document.querySelectorAll('#' + componentSocketId + ' zeron-a');
     forEach(arr, el => {
-        el.style.cursor = "pointer";
         el.addEventListener('click', () => { 
             const path = el.getAttribute('href');
-            route().pushState(path);
+            if (path !== window.location.pathname) {
+                route().pushState(path);
+            };
         });
     })
 }
