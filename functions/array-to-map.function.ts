42ae178047ff011c10dd22e81fbfc57d7c14c3de
Benjamin Clark <benjaminclark@Benjamins-Mac-mini.local>
2018-10-20 07:06:31 +1030

    fix value label

--- functions/array-to-map.function.ts
+++ functions/array-to-map.function.ts
@@ -1,7 +1,7 @@
 export function arrayToMap(arr, fn) {
     return arr.reduce((ac, cv) => {
         const obj = fn(cv);
-        const value = typeof obj.value !== 'object' ? { item: obj.value } : obj.value;
+        const value = typeof obj.value !== 'object' ? { value: obj.value } : obj.value;
         const mergeValue = Object.assign({}, value, { key: obj.key });
         return Object.assign({}, ac, { [obj.key]: mergeValue });
     }, {});
