82d72a22ecb51155ecfd50ee02fda193d8cd1346
Benjamin Clark <benjaminclark@Benjamins-Mac-mini.local>
2018-10-20 07:35:58 +1030

    fix

--- functions/array-to-map.function.ts
+++ functions/array-to-map.function.ts
@@ -1,8 +1,8 @@
 export function arrayToMap(arr, fn) {
     return arr.reduce((ac, cv) => {
-        const obj = fn(cv);
-        const value = typeof obj.value !== 'object' ? { value: obj.value } : obj.value;
-        const mergeValue = Object.assign({}, value, { key: obj.key });
-        return Object.assign({}, ac, { [obj.key]: mergeValue });
+      const obj = fn(cv);
+      const value = { value: obj.value };
+      const mergeValue = Object.assign({}, value, {key: obj.key });
+      return Object.assign({}, ac, { [obj.key]: mergeValue });
     }, {});
-}
+  }
